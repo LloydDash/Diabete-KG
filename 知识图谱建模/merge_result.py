43d9316fae7f2ec44430311f79bda005c7f96c46
LloydDash <158806074+LloydDash@users.noreply.github.com>
2024-05-18 11:30:04 +0800

    Update merge_result.py

--- 知识图谱建模/merge_result.py
+++ 知识图谱建模/merge_result.py
@@ -18,4 +18,4 @@ df=pd.concat([y_diabetes,y_not_diabetes],axis=1,join='inner',ignore_index=True)
 # df.fillna(method = 'ffill', axis = 1) # 将通过前向填充 (ffill) 方法用同一行的前一个数作为填充
 print(df)
 
-pd.DataFrame(df).to_excel(os.path.join(cur_dir, 'train\\pred_result.xlsx'))
+pd.DataFrame(df).to_excel(os.path.join(cur_dir, 'data\\pred_result.xlsx'))
